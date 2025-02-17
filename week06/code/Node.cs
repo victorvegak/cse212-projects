8fffc059199d5f48a0f0df795dd705c3fda4d10f
victorvegak <vvega28@outlook.com>
2025-02-17 00:40:42 -0600

    week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,7 +12,12 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data) 
+        {
+            return;
+        }
 
+    
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +39,27 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if(value == Data)
+        {
+            return true;
+        }
+        if(value < Data)
+        {
+            return Left?.Contains(value) ?? false;
+        }
+        return Right?.Contains(value) ?? false;
+
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (this == null)
+            return 0;
+
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight); // Replace this line with the correct return statement(s)
     }
 }
